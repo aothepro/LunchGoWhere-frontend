5cd4ac8481fe454344a7941d2777c85b78e806bd
Ashraf Omar <ao356@live.com>
2023-07-22 20:01:21 +0800

    initial commit

---
+++ src/components/SessionsTable.tsx
@@ -0,0 +1,3 @@
+export default function SessionsTable() {
+  return <div className="">Session table Page</div>;
+}
---
+++ src/components/VotePage.tsx
@@ -0,0 +1,3 @@
+export default function VotePage() {
+  return <div className="">Vote Page</div>;
+}

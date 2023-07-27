06dfb6562c24e3d113c5850f79d3ed18692cefa0
Ashraf Omar <ao356@live.com>
2023-07-27 18:39:38 +0800

    added no cache to fetch requests

--- src/utilities/fetch.tsx
+++ src/utilities/fetch.tsx
@@ -6,7 +6,7 @@ const Authorization = "Bearer " + getCookie(JWT_COOKIE_NAME);
 export const get = (input: RequestInfo | URL, init?: RequestInit | undefined) =>
   fetch(input, {
     method: "GET",
-    headers: { Authorization },
+    headers: { Authorization, cache: "no-cache" },
     ...init,
   });
 
@@ -17,7 +17,11 @@ export const post = (
 ) =>
   fetch(input, {
     method: "POST",
-    headers: { "Content-Type": "application/json", Authorization },
+    headers: {
+      "Content-Type": "application/json",
+      Authorization,
+      cache: "no-cache",
+    },
     body: JSON.stringify(body),
     ...init,
   });

3bed6866ac096c10c7fc1351aad65e93e71fd1e4
Ashraf Omar <ao356@live.com>
2023-07-27 21:31:35 +0800

    created home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <div className="">Home Page</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen p-24">
+      <div className="m-auto block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Welcome to LunchGoWhere
+        </h5>
+        <p className="p-5 flex justify-between font-normal text-gray-700 dark:text-gray-400">
+          <button
+            onClick={() => navigate("/login")}
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Login
+            </span>
+          </button>
+          <button
+            onClick={() => navigate("/register")}
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Register
+          </button>
+        </p>
+      </div>
+    </div>
+  );
 }
